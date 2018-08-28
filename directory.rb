bdf8d5f69618ea57aad04dc5fcb53cbc0503c753
Laura Jackson <playeronereadystart@gmail.com>
2018-08-28 11:41:27 +0100

    Refactor nested array into array of hashes

--- directory.rb
+++ directory.rb
@@ -1,21 +1,21 @@
 students = [
-  "Misty",
-  "Daisy",
-  "Lily",
-  "Violet",
-  "Ash Ketchum",
-  "Aquaman",
-  "Jar Jar Binks",
-  "Ariel",
-  "Melody"
+  {name: "Misty",  status: :current},
+  {name: "Daisy", status: :current},
+  {name: "Lily", status: :current},
+  {name: "Violet", status: :current},
+  {name: "Ash Ketchum", status: :current},
+  {name: "Aquaman", status: :previous},
+  {name: "Jar Jar Binks", status: :previous},
+  {name: "Ariel", status: :previous},
+  {name: "Melody", status: :current}
 ]
 def print_header
   puts "The students of Cerulean City Gym"
   puts "--------------"
 end
-def print(names)
-  names.each do |name|
-    puts name
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (Student status: #{student[:status]})"
   end
 end
 def print_footer(names)
