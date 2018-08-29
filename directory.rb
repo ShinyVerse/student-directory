503799b09f9a56752a0d8958fd682418dba26b42
Laura Jackson <playeronereadystart@gmail.com>
2018-08-29 12:03:18 +0100

    Add method to load student list

--- directory.rb
+++ directory.rb
@@ -4,6 +4,7 @@ def print_menu
   puts "[1] Input students"
   puts "[2] Show the students"
   puts "[3] Save the list to students.csv"
+  puts "[4] Load the list from students.csv"
   puts "[9] Exit"
 end
 
@@ -25,6 +26,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -96,6 +99,15 @@ def save_students
     student_data = [student[:name], student[:status], student[:fav_pokemon]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, status, fav_pokemon = line.chomp.split(",")
+    @students << {name: name, status: status.to_sym, fav_pokemon: fav_pokemon}
   end
   file.close
 end
