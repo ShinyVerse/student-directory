deeac71ab34fa2785c42a1d46584a5ba077b2508
Laura Jackson <playeronereadystart@gmail.com>
2018-08-29 13:39:35 +0100

    Add try_load_students method

--- directory.rb
+++ directory.rb
@@ -56,14 +56,14 @@ def input_students
   puts "To finish, enter command 'stop'"
   while true do
     puts "Name: "
-    name = gets.chomp
+    name = STDIN.gets.chomp
     break if input_break_loop?(name)
     puts "Status: "
-    status = gets.chomp
+    status = STDIN.gets.chomp
     break if input_break_loop?(status)
     puts "Favourite Pokemon: "
     # Use of someting other than chomp.
-    pokemon = gets.strip
+    pokemon = STDIN.gets.strip
     break if input_break_loop?(pokemon)
     if name.empty?
       name = "Unknown"
@@ -103,8 +103,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename ="students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, status, fav_pokemon = line.chomp.split(",")
     @students << {name: name, status: status.to_sym, fav_pokemon: fav_pokemon}
@@ -112,10 +112,23 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist"
+    exit
+  end
+end
+
 def interactive_menu
+  try_load_students
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
