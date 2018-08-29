918d48872cffb01796164a7917eeca75877fae16
Laura Jackson <playeronereadystart@gmail.com>
2018-08-29 11:04:41 +0100

    Change to using variable @students

--- directory.rb
+++ directory.rb
@@ -1,39 +1,24 @@
-def print_header
-  puts "The students of Cerulean City Gym"
-  puts "--------------".center(30)
-end
-def print_students(students)
-  students.each_with_index do |student, index|
+@students = []
+
+def print_students
+  @students.each_with_index do |student, index|
       puts "#{index + 1}. #{student[:name]}".center(30)
       puts "Student status: #{student[:status]}".center(30)
-      puts "Favourite Pokemon: #{student[:fav_pokemon]}\n".center(30)
+      puts "Favourite Pokemon: #{student[:fav_pokemon]}".center(30)
+      puts "\n"
     end
-end
-
-def print_footer(names)
-  puts "\nOverall, we have #{names.count} great " + (names.count > 1 ? "students\n" : "student\n")
 end
 
 def input_break_loop?(entry)
    return true if entry == 'stop'
 end
 
-def print_by_status(students, status)
-  returned_collection = []
-  students.map do | student |
-      if student[:status] == status
-        returned_collection.push(student)
-      end
-  end
-  print_students(returned_collection)
-end
-
 def input_students
+  local_students = []
   puts "Please enter the names of the students"
   puts "Followed by the current status"
   puts "Then a favourite Pokemon"
   puts "To finish, enter command 'stop'"
-  students = []
   while true do
     puts "Name: "
     name = gets.chomp
@@ -54,19 +39,26 @@ def input_students
     if pokemon.empty?
       pokemon = "Jigglypuff"
     end
-    students << {name: name, status: status.to_sym, fav_pokemon: pokemon}
-    puts "Now we have #{students.count} " + (students.count == 1?  "student\n" : "students\n")
+    local_students << {name: name, status: status.to_sym, fav_pokemon: pokemon}
+    puts "Now we have #{local_students.count} " + (local_students.count == 1?  "student\n" : "students\n")
   end
-  if students.length > 0
-    return students
+  if local_students.length > 0
+     @students = local_students
   else
-    return students = []
+    @students = []
   end
 end
 
+def print_header
+  puts "The students of Cerulean City Gym"
+  puts "--------------".center(30)
+end
+
+def print_footer
+  puts "\nOverall, we have #{@students.count} great " + (@students.count > 1 ? "students\n" : "student\n")
+end
 
 def interactive_menu
-  students = []
   loop do
     # 1. print the menu and ask the user what to do
     puts "[1] Input students"
@@ -77,12 +69,12 @@ def interactive_menu
     # 3. do what the user has asked
     case selection
     when "1"
-      students = input_students
+      @students = input_students
     when "2"
-      if !students.empty?
+      if !@students.empty?
         print_header
-        print_students(students.clone)
-        print_footer(students)
+        print_students
+        print_footer
       else
         puts "\nThere are no students yet :("
       end
